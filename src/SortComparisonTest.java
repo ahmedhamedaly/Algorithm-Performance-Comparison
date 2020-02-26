72b5fc1bdfdd2fcddff65ccc5dfbf921a8c7f6d3
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-26 09:26:27 +0000

    added tests for all the algorithms

--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.Arrays;
+
 //-------------------------------------------------------------------------
 /**
  *  Test class for SortComparison.java
@@ -44,27 +46,67 @@ public class SortComparisonTest
 
     @Test
     public void insertionSortTest() {
+        double[] a = {
+                3431.21, 7283.93, 2846.63, 4639.46, 8446.73, 9142, 7832.26, 6482.50, 5392.01, 9201.64
+        };
 
+        double[] result = {
+                2846.63, 3431.21, 4639.46, 5392.01, 6482.5, 7283.93, 7832.26, 8446.73, 9142.0, 9201.64
+        };
+
+        assertEquals(Arrays.toString(result), Arrays.toString(SortComparison.insertionSort(a)));
     }
 
     @Test
     public void selectionSortTest() {
+        double[] a = {
+                3431.21, 7283.93, 2846.63, 4639.46, 8446.73, 9142, 7832.26, 6482.50, 5392.01, 9201.64
+        };
 
+        double[] result = {
+                2846.63, 3431.21, 4639.46, 5392.01, 6482.5, 7283.93, 7832.26, 8446.73, 9142.0, 9201.64
+        };
+
+        assertEquals(Arrays.toString(result), Arrays.toString(SortComparison.selectionSort(a)));
     }
 
     @Test
     public void quickSortTest() {
+        double[] a = {
+                3431.21, 7283.93, 2846.63, 4639.46, 8446.73, 9142, 7832.26, 6482.50, 5392.01, 9201.64
+        };
 
+        double[] result = {
+                2846.63, 3431.21, 4639.46, 5392.01, 6482.5, 7283.93, 7832.26, 8446.73, 9142.0, 9201.64
+        };
+
+        assertEquals(Arrays.toString(result), Arrays.toString(SortComparison.quickSort(a)));
     }
 
     @Test
     public void mergeSortIterativeTest() {
+        double[] a = {
+                3431.21, 7283.93, 2846.63, 4639.46, 8446.73, 9142, 7832.26, 6482.50, 5392.01, 9201.64
+        };
 
+        double[] result = {
+                2846.63, 3431.21, 4639.46, 5392.01, 6482.5, 7283.93, 7832.26, 8446.73, 9142.0, 9201.64
+        };
+
+        assertEquals(Arrays.toString(result), Arrays.toString(SortComparison.mergeSortIterative(a)));
     }
 
     @Test
     public void mergeSortRecursive() {
+        double[] a = {
+                3431.21, 7283.93, 2846.63, 4639.46, 8446.73, 9142, 7832.26, 6482.50, 5392.01, 9201.64
+        };
 
+        double[] result = {
+                2846.63, 3431.21, 4639.46, 5392.01, 6482.5, 7283.93, 7832.26, 8446.73, 9142.0, 9201.64
+        };
+
+        assertEquals(Arrays.toString(result), Arrays.toString(SortComparison.mergeSortRecursive(a)));
     }
 
     // ----------------------------------------------------------
@@ -73,8 +115,7 @@ public class SortComparisonTest
      *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
      *
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //TODO: implement this method
     }
 
