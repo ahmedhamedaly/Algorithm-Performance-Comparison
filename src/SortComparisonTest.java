6374e53e0fb6c0e60af3eca2efbff53c95280413
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-26 17:24:50 +0000

    finished questions answer

--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -17,18 +17,40 @@ import java.util.Scanner;
  *  @author Ahmed Hamed Aly
  *  @version HT 2020
  *
- * RUNNING TIME PER DATA SAMPLE AND ALGORITHM
+ * RUNNING TIME PER DATA SAMPLE AND ALGORITHM (in nano seconds)
  *
- * |                    | INSERT | SELECTION | QUICK | MERGE (I) | MERGE (R) |
- * |--------------------|--------|-----------|-------|-----------|-----------|
- * | 10 RANDOM          |   --   | YES       | YES   | YES       | YES       |
- * | 100 RANDOM         |  YES   | YES       | YES   | YES       | YES       |
- * | 1000 RANDOM        |  YES   | YES       | YES   | YES       | YES       |
- * | 1000 FEW UNIQUE    |  YES   | YES       | YES   | --        | YES       |
- * | 1000 NEARLY ORDERED|  YES   | YES       | YES   | YES       | YES       |
- * | 1000 REVERSE ORDER |  YES   | YES       | YES   | YES       | YES       |
- * | 1000 SORTED        |  YES   | YES       | YES   | YES       | YES       |
- * |-------------------------------------------------------------------------|
+ * |                    |  INSERT  | SELECTION |  QUICK  | MERGE (I) | MERGE (R) |
+ * |--------------------|----------|-----------|---------|-----------|-----------|
+ * | 10 RANDOM          | 1190330  | 13559     | 26022   | 111741    | 36088     |
+ * | 100 RANDOM         | 197208   | 336987    | 406614  | 96061     | 105156    |
+ * | 1000 RANDOM        | 13935228 | 10778625  | 10885717| 1545218   | 1367870   |
+ * | 1000 FEW UNIQUE    | 488883   | 2813942   | 1570501 | 464324    | 338007    |
+ * | 1000 NEARLY ORDERED| 78165    | 2815423   | 10860034| 238831    | 326655    |
+ * | 1000 REVERSE ORDER | 952969   | 2812003   | 3284062 | 297403    | 197902    |
+ * | 1000 SORTED        | 9716     | 2800932   | 1755763 | 246510    | 152136    |
+ * |-----------------------------------------------------------------------------|
+ *
+ * QUESTIONS:
+ * a.  Which of the sorting algorithms does the order of input have an impact on? Why?
+ * ANSWER: Insertion Sort because the best case is Theta(n) and the worse case is Theta(n^2)
+ *
+ * b.  Which algorithm has the biggest difference between the best and worst performance, based on the type of input, for the input of size 1000? Why?
+ * ANSWER: Insertion Sort has the biggest difference (from Theta(n) to Theta(n^2)
+ *
+ * c.  Which algorithm has the best/worst scalability, i.e.  , the difference in performance time based on the input size? Please consider only input files with random order for this answer.
+ * ANSWER: Merge Sort Recursive as it has the best scalability and remains the same with its 0(n log(n)) runtime and wont be affected by input size
+ *
+ * d.  Did you observe any difference between iterative and recursive implementations of merge sort?
+ * ANSWER: One is called over and over again recursively and one is iteratively. From my results the recursive merge sort ran faster.
+ *
+ * e.  Which algorithm is the fastest for each of the 7 input files?
+ * ANSWER: 10 random: Selection Sort
+ *         100 random: Merge Sort Iteration
+ *         1000 random: Merge Sort Recursive
+ *         1000 few unique: Merge Sort Recursive
+ *         1000 nearly ordered: Insertion Sort
+ *         1000 reverse order: Merge Sort Recursive
+ *         1000 sorted: Insertion
  */
 
 @RunWith(JUnit4.class)
@@ -149,6 +171,8 @@ public class SortComparisonTest {
             File file1 = new File(file);
             inputScanner = new Scanner(file1);
             ArrayList<String> doublesList = new ArrayList<>();
+            long start, end, time;
+            String dataTitle;
 
             while (inputScanner.hasNextLine())
                 doublesList.add(inputScanner.nextLine());
@@ -158,15 +182,38 @@ public class SortComparisonTest {
                 a[i] = Double.parseDouble(doublesList.get(i));
             }
 
-            String output_title = file.replaceAll("testFiles/", "").replaceAll(".txt", "");
+            dataTitle = file.replaceAll("testFiles/", "").replaceAll(".txt", "");
 
-            long start = System.nanoTime();
-
+            System.out.println("---- " + dataTitle + " ----");
+            start = System.nanoTime();
             SortComparison.insertionSort(a);
+            end = System.nanoTime();
+            time = end - start;
+            System.out.println("Insertion Sort completed with a time of " + time);
 
-            long end = System.nanoTime();
+            start = System.nanoTime();
+            SortComparison.selectionSort(a);
+            end = System.nanoTime();
+            time = end - start;
+            System.out.println("Selection Sort completed with a time of " + time);
 
-            System.out.println(start/end);
+            start = System.nanoTime();
+            SortComparison.quickSort(a);
+            end = System.nanoTime();
+            time = end - start;
+            System.out.println("Quick Sort completed with a time of " + time);
+
+            start = System.nanoTime();
+            SortComparison.mergeSortIterative(a);
+            end = System.nanoTime();
+            time = end - start;
+            System.out.println("Merge Sort Iterative completed with a time of " + time);
+
+            start = System.nanoTime();
+            SortComparison.mergeSortRecursive(a);
+            end = System.nanoTime();
+            time = end - start;
+            System.out.println("Merge Sort Recursive completed with a time of " + time + "\n");
 
         }
     }
